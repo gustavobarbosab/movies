31e3519378015f428fa6034b78e5c10f4bc5c90e
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2021-05-22 17:19:08 -0300

    criando estrutura do navigation da home

--- features/home/src/main/java/io/github/gustavobarbosab/home/HomeFragment.kt
+++ features/home/src/main/java/io/github/gustavobarbosab/home/HomeFragment.kt
@@ -1,6 +1,35 @@
 package io.github.gustavobarbosab.home
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import io.github.gustavobarbosab.home.databinding.FragmentHomeBinding
 
-class HomeFragment: Fragment() {
+class HomeFragment : Fragment() {
+
+    var binding: FragmentHomeBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.bottomNav?.setupWithNavController(findNavController())
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
 }
