1f2f4be804311b2c3b90398c1933b1b4bd7097d3
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2021-05-22 13:34:07 -0300

    Adicionando dependencias nas features

---
+++ app/src/main/java/io/github/gustavobarbosab/movies/MovieApplication.kt
@@ -0,0 +1,6 @@
+package io.github.gustavobarbosab.movies
+
+import android.app.Application
+
+class MovieApplication: Application() {
+}
---
+++ app/src/main/java/io/github/gustavobarbosab/movies/di/MovieModule.kt
@@ -0,0 +1,3 @@
+package io.github.gustavobarbosab.movies.di
+
+class MovieModule
