d22767bc9fd7afe9e1c310b99b2849756bb19cd0
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2021-05-27 07:50:14 -0300

    ajustando navegacao e bottomnavigation

--- app/src/main/java/io/github/gustavobarbosab/movies/MainActivity.kt
+++ app/src/main/java/io/github/gustavobarbosab/movies/MainActivity.kt
@@ -1,6 +1,7 @@
 package io.github.gustavobarbosab.movies
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.NavHostFragment
@@ -17,14 +18,25 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
 
     var navController = lazy {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_main_fragment) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_main_fragment) as NavHostFragment
         navHostFragment.navController
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupBottomNav()
+    }
+
+    private fun setupBottomNav() {
         binding.bottomNav.setupWithNavController(navController.value)
+        navController.value.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bottomNav.visibility = when (destination.id) {
+                R.id.fragment_list, R.id.fragment_favorite -> View.VISIBLE
+                else -> View.GONE
+            }
+        }
     }
 
     override fun onSupportNavigateUp() =
