bdda9572a2f116c4dcb1c2529fe68a8c47c285ea
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2021-10-18 09:47:02 -0300

    Commiting details package rename

---
+++ features/detail/src/main/java/io/github/gustavobarbosab/movies/movies/detail/MovieDetailFragment.kt
@@ -0,0 +1,34 @@
+package io.github.gustavobarbosab.movies.movies.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.play.core.splitcompat.SplitCompat
+import io.github.gustavobarbosab.detail.R
+import io.github.gustavobarbosab.movies.extension.applicationToolbar
+
+class MovieDetailFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        SplitCompat.install(requireContext())
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movie_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        applicationToolbar {
+            title("Details")
+            backButton(true)
+        }
+    }
+}
