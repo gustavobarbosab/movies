c9823aafee56a532a7185ef92cc87759f0bbceaa
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2021-11-10 04:59:46 -0300

    improving showcase dependencies

--- features/showcase/showcase_data/src/main/java/io/github/gustavobarbosab/movies/data/di/ShowCaseDataModule.kt
+++ features/showcase/showcase_data/src/main/java/io/github/gustavobarbosab/movies/data/di/ShowCaseDataModule.kt
@@ -2,14 +2,7 @@ package io.github.gustavobarbosab.movies.data.di
 
 import dagger.Module
 import dagger.Provides
-import io.github.gustavobarbosab.core.data.config.AppConfigWrapper
-import io.github.gustavobarbosab.core.data.database.movie.MovieDao
-import io.github.gustavobarbosab.core.data.network.services.movies.MovieApi
 import io.github.gustavobarbosab.movies.data.ShowCaseRepositoryImpl
-import io.github.gustavobarbosab.movies.data.ShowCaseRepositoryMapper
-import io.github.gustavobarbosab.movies.data.datasources.local.ShowCaseLocalDataSource
-import io.github.gustavobarbosab.movies.data.datasources.remote.ShowCaseRemoteDataSource
-import io.github.gustavobarbosab.showcase.repository.ShowCaseRepository
 import io.github.gustavobarbosab.showcase.usecase.ShowCaseUseCase
 import io.github.gustavobarbosab.showcase.usecase.ShowCaseUseCaseImpl
 
@@ -17,29 +10,6 @@ import io.github.gustavobarbosab.showcase.usecase.ShowCaseUseCaseImpl
 class ShowCaseDataModule {
 
     @Provides
-    fun provideUseCase(repository: ShowCaseRepository): ShowCaseUseCase =
+    fun provideUseCase(repository: ShowCaseRepositoryImpl): ShowCaseUseCase =
         ShowCaseUseCaseImpl(repository)
-
-    @Provides
-    fun provideMovieRepository(
-        localDataSource: ShowCaseLocalDataSource,
-        remoteDataSource: ShowCaseRemoteDataSource,
-        mapper: ShowCaseRepositoryMapper
-    ): ShowCaseRepository = ShowCaseRepositoryImpl(
-        localDataSource,
-        remoteDataSource,
-        mapper
-    )
-
-    @Provides
-    fun provideLocalMovieDataSource(movieDao: MovieDao): ShowCaseLocalDataSource =
-        ShowCaseLocalDataSource(movieDao)
-
-    @Provides
-    fun provideRemoteMovieDataSource(api: MovieApi): ShowCaseRemoteDataSource =
-        ShowCaseRemoteDataSource(api)
-
-    @Provides
-    fun provideMapper(configWrapper: AppConfigWrapper) =
-        ShowCaseRepositoryMapper(configWrapper)
 }
