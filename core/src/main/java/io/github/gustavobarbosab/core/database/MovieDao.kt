c5dce2c105a5408a5298bdbde10b7cd6ac34bcf4
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2021-11-14 14:50:32 -0300

    creating database

--- core/src/main/java/io/github/gustavobarbosab/core/database/MovieDao.kt
+++ core/src/main/java/io/github/gustavobarbosab/core/database/MovieDao.kt
@@ -1,3 +1,20 @@
 package io.github.gustavobarbosab.core.database
 
-class MovieDao
+import androidx.room.*
+import io.github.gustavobarbosab.core.database.dto.MovieDto
+
+@Dao
+interface MovieDao {
+
+    @Query("SELECT * FROM Movie")
+    fun getAll(): List<MovieDto>
+
+    @Query("SELECT * FROM Movie WHERE favorite ORDER BY savedDate")
+    fun getFavorites(): List<MovieDto>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertMovie(vararg movieDto: MovieDto)
+
+    @Query("UPDATE Movie SET favorite = 'false' WHERE id = :id")
+    fun unlikeMovie(id: Long)
+}
--- core/src/main/java/io/github/gustavobarbosab/core/di/modules/CoreModule.kt
+++ core/src/main/java/io/github/gustavobarbosab/core/di/modules/CoreModule.kt
@@ -1,9 +1,11 @@
 package io.github.gustavobarbosab.core.di.modules
 
+import android.content.Context
+import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import io.github.gustavobarbosab.core.config.AppConfigWrapper
-import io.github.gustavobarbosab.core.database.MovieDao
+import io.github.gustavobarbosab.core.database.MoovieDataBase
 import io.github.gustavobarbosab.core.network.services.movies.MovieApi
 import retrofit2.Retrofit
 import javax.inject.Singleton
@@ -21,5 +23,14 @@ class CoreModule {
 
     @Provides
     @Singleton
-    fun provideMovieDao(): MovieDao = MovieDao()
+    fun provideMovieDataBase(applicationContext: Context) =
+        Room.databaseBuilder(
+            applicationContext,
+            MoovieDataBase::class.java,
+            "moovie-database"
+        ).build()
+
+    @Provides
+    @Singleton
+    fun provideMovieDao(dataBase: MoovieDataBase) = dataBase.movieDao()
 }
