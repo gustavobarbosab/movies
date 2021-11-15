aa9f6ada57afe9cafa7689517f243851de5abdda
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2021-11-15 10:52:49 -0300

    fixing progress error

--- commons/src/main/java/io/github/gustavobarbosab/commons/widget/progress/MoovieLoading.kt
+++ commons/src/main/java/io/github/gustavobarbosab/commons/widget/progress/MoovieLoading.kt
@@ -2,21 +2,27 @@ package io.github.gustavobarbosab.commons.widget.progress
 
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
-import androidx.lifecycle.LifecycleOwner
 
 class MoovieLoading {
 
     fun showLoading(fragmentManager: FragmentManager) {
-        hideLoading(fragmentManager)
+        val dialogProgress = getFragmentProgress(fragmentManager)?.dialog
+        if (dialogProgress?.isShowing == true)
+            return
+
         MoovieProgressDialogFragment().show(fragmentManager, TAG)
     }
 
     fun hideLoading(fragmentManager: FragmentManager) {
-        fragmentManager.findFragmentByTag(TAG)
-            ?.let { (it as DialogFragment).dismissAllowingStateLoss() }
+        val fragmentProgress = getFragmentProgress(fragmentManager)
+        fragmentProgress?.let {
+            it.dialog?.cancel()
+            it.dismissAllowingStateLoss()
+        }
     }
+
+    private fun getFragmentProgress(fragmentManager: FragmentManager) =
+        fragmentManager.findFragmentByTag(TAG) as? DialogFragment?
 
     companion object {
         private const val TAG = "TAG_LOADING"
